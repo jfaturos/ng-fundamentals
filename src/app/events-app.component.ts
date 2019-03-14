d1840650f57283f92c0c7fb6416e6ac03e877faa
Giannis Fatouros <fatouros.ioannis@gmail.com>
2019-03-14 20:47:59 +0200

    Bootstrapping an Angular App

---
+++ src/app/events-app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'events-app',
+  template: '<h2>Hello World!</h2>',
+  styleUrls: ['./events-app.component.css']
+})
+export class EventsAppComponent {
+  title = 'ng-fundamentals';
+}
